9658a9042aaa1f32a0f922cc570076337e3e661f
Lessa Apolinario <lessaapolinario@gmail.com>
2023-05-13 20:07:24 -0300

    Add loading component for products

--- src/ui/frontend/pages/HomePage/index.tsx
+++ src/ui/frontend/pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import styles from './styles.module.scss';
 import { useNavigate } from 'react-router-dom';
 import { Header } from '../../components/Header';
-import { ProductCard } from '../../components/productCard';
+import { Loading } from '../../components/Loading';
+import { GenericList } from '../../components/GenericList';
 import { Product } from '../../../../core/domain/models/Product';
+import { ProductCard } from '../../components/productCard';
 
 function HomePage() {
   const navigate = useNavigate();
@@ -12,20 +14,30 @@ function HomePage() {
     navigate('/home');
   }, [navigate]);
 
+  function renderProduct(product: Product) {
+    return <ProductCard key={product.id} data={product} />;
+  }
+
   return (
     <div className={styles.container}>
       <Header text="Página de produtos" />
 
       <main>
-        <ProductCard
-          data={Product.fromJSON({
-            name: 'Mouse simples',
-            description:
-              'Mouse com fio e led vermelho, ideal para usuários comuns.',
-            price: 22.9,
-            quantity: 20,
-          })}
-        />
+        <Suspense fallback={<Loading text="Carregando produtos..." />}>
+          <GenericList<Product>
+            className={styles.products}
+            data={[
+              Product.fromJSON({
+                name: 'A',
+                description:
+                  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, architecto, totam nisi id et aliquam, accusantium perferendis provident iure error quidem debitis praesentium. Porro recusandae a est consequuntur molestiae cumque.',
+                price: 22.9,
+                quantity: 20,
+              }),
+            ]}
+            renderItem={renderProduct}
+          />
+        </Suspense>
       </main>
     </div>
   );
